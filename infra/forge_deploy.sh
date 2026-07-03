c356356fd243b6df0741df0a33df4e7bf3a1af2b
j_a_ <jerome.arfouche@pm.me>
2026-07-03 08:05:19 -0400

    deploy fix

--- infra/forge_deploy.sh
+++ infra/forge_deploy.sh
@@ -2,9 +2,9 @@ $CREATE_RELEASE()
 
 cd $FORGE_RELEASE_DIRECTORY
 
-/home/forge/.local/share/pnpm/bin/pnpm install --frozen-lockfile
-/home/forge/.local/share/pnpm/bin/pnpm build
-/home/forge/.local/share/pnpm/bin/pnpm db:migrate
+$PNPM_PATH install --frozen-lockfile
+$PNPM_PATH build
+$PNPM_PATH db:migrate
 
 $ACTIVATE_RELEASE()
 
